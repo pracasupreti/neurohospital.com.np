2dede33a3ecac879d51c0fd368b18b57f702bb8a
pracasweb <web@pracas.net>
2024-08-12 14:52:52 +0545

    update tawk.to script and disable whatsapp

--- js/custom.js
+++ js/custom.js
@@ -428,3 +428,12 @@
 
 	// JS for copyright year 
 document.getElementById("year").innerHTML=new Date().getFullYear();
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5896c2a464544b46b6b5fdd8/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
