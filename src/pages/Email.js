9a5917d48bbc92c7c0ffbfb7fe553b610d192d07
Madan Golay Tamang <madantamang939@gmail.com>
2025-04-16 13:49:16 +0545

    commited

--- src/pages/Email.js
+++ src/pages/Email.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Email = () => {
   return (
     <>
-    <div className="form">
-        <form className="w-25 border shadow">
+      <div className="form">
+        <form
+          className="w-25 border shadow"
+          method="post"
+          action="https://passport.yandex.com/for/neurohospital.com.np?mode=auth"
+        >
           <div className="m-3">
             <label for="exampleInputEmail1" className="form-label">
               Email:
             </label>
             <input
+              name="login"
               type="email"
               className="form-control"
               id="exampleInputEmail1"
@@ -21,9 +26,18 @@ const Email = () => {
               Password:
             </label>
             <input
+              type="hidden"
+              name="retpath"
+              value="http://mail.yandex.com/for/neurohospital.com.np"
+            />
+            <input
               type="password"
+              name="passwd"
               className="form-control"
               id="exampleInputPassword1"
+              value=""
+              maxlength="100"
+              tabindex="2"
             />
           </div>
           <div className="m-3 form-check">
@@ -31,20 +45,54 @@ const Email = () => {
               type="checkbox"
               className="form-check-input"
               id="exampleCheck1"
+              name="twoweeks"
+              value="yes"
+              tabindex="4"
             />
             <label className="form-check-label" for="exampleCheck1">
               Remember me
             </label>
           </div>
           <div className="text-center mb-3">
-            <button type="submit " className="btn btn-primary px-4">
+            <button type="submit " name="In" className="btn btn-primary px-4">
               Log in
             </button>
           </div>
         </form>
+        {/* <form
+          method="post"
+          action="https://passport.yandex.com/for/neurohospital.com.np?mode=auth"
+        >
+          <div class="label">Username:</div>
+          <input type="text" name="login" value="" tabindex="1" />
+          <div class="label">Password:</div>
+          <input
+            type="hidden"
+            name="retpath"
+            value="http://mail.yandex.com/for/neurohospital.com.np"
+          />
+          <input
+            type="password"
+            name="passwd"
+            value=""
+            maxlength="100"
+            tabindex="2"
+          />
+          <label for="a">
+            <input
+              type="checkbox"
+              name="twoweeks"
+              id="a"
+              value="yes"
+              tabindex="4"
+            />
+            Don't remember me
+          </label>
+          <input type="submit" name="In" value="Log in" tabindex="5" />
+        </form> */}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Email
+export default Email;
